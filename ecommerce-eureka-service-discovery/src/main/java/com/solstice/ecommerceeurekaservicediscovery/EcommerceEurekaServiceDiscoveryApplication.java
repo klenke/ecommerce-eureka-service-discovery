ff5c95bbc9d7a5ac7a1d105e6f4ad329fea3c372
Collin Klenke <collinklenke@Collin-Klenkes-MacBook-Pro.local>
2018-08-31 09:50:14 -0500

    first commit

---
+++ ecommerce-eureka-service-discovery/src/main/java/com/solstice/ecommerceeurekaservicediscovery/EcommerceEurekaServiceDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.solstice.ecommerceeurekaservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceEurekaServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceEurekaServiceDiscoveryApplication.class, args);
+	}
+}
